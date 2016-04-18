be4c2beef492af2b0fa1d19158fd46cdac682740
Cédric Verstraeten <hello@cedric.ws>
2016-04-18 11:42:37 +0200

    add language support for dutch

--- app/lang/fr/cloud.php
+++ app/lang/fr/cloud.php
@@ -15,6 +15,6 @@ return array(
     
     'description' => 'Kerberos.io fournit un <b>service cloud</b>, qui permet de centraliser et suivre plusieurs instances Kerberos.io. Pour synchroniser les évènements de cette instance avec le cloud, vous aurez d\'abord besoin de <a href="https://cloud.kerberos.io">créer un compte</a> et de souscrire à un abonnement. Une fois l\'inscription terminée, vous recevrez les informations permettant de remplir le formulaire ci-dessous. Quand l\'authentification est validée, les évènements sont alors envoyés à l\'application cloud.',
     
-	'update' => 'Update',
+	'update' => 'Sauvegarder',
 
 );
---
+++ app/lang/nl/system.php
@@ -0,0 +1,80 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| System Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the system page.
+	|
+	*/
+
+	'system' => 'Systeem',
+
+	'diskAlmostFull' => 'Hey, je hardeschijf is bijna vol. Verwijder enkele beelden..',
+
+	'board' => 'Board',
+    
+	'model' => 'Model',
+    
+	'os' => 'OS',
+    
+	'kernel' => 'Kernel',
+    
+	'hostname' => 'Hostname',
+    
+	'architecture' => 'Architectuur',
+    
+	'thisMachine' => 'Deze machine heeft',
+    
+	'cpuRunning' => 'CPU\'s aanwezig met een gemiddelde belasting van',
+    
+	'diskSpecs' => 'Schijf specificaties',
+    
+	'thereAre' => 'Er zijn',
+    
+	'harddisksAvailable' => 'partities aanwezig op dit systeem',
+    
+	'networkSpecs' => 'Netwerk specificaties',
+    
+		'device' => 'Device',
+    
+		'recieved' => 'Ontvangen',
+    
+		'sent' => 'Verstuurd',
+    
+	'twoServicesRunning' => 'heeft twee applicaties actief: de web en de machinery',
+    
+	'versions' => 'Versies',
+    
+	'numberOfImages' => '# beelden',
+    
+	'numberOfDays' => '# dagen',
+    
+	'days' => 'Dagen',
+    
+	'downloadSystemFiles' => 'Download systeem bestanden',
+    
+	'downloadImages' => 'Download beelden',
+    
+	'removeImages' => 'Verwijder beelden',
+    
+	'release' => 'Release',
+    
+	'publishedAt' => 'Gepubliceerd op',
+    
+	'install' => 'Installeren',
+    
+	'downloading' => 'Downloaden',
+    
+	'unzipping' => 'Decrompimeren',
+    
+	'unpacking' => 'Uitpakken',
+    
+	'transferring' => 'Schrijven',
+    
+	'rebooting' => 'Systeem is aan het heropstarten',
+
+);
