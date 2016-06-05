6c8e2170d2ba6fd685ab3d3e45a9f3526ae1c9e1
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-06-05 16:38:03 +0200

    configurabl stream port

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -98,16 +98,20 @@
                                  ], 
                         function(Streamer, Sequencer, Heatmap, Pie, Graph, Radar)
                         {
-                            Streamer.initialize(
-                            {
-                                element: "livestream",
-                                host: "<?=parse_url(URL::to('/'), PHP_URL_HOST)?>",
-                                port: 8888,
-                                width: '100%',
-                                callback: function(){}
-                            });
-                            
-                            Sequencer.initialize(
+		         $.get('api/v1/streamPort',function(data)
+                         {
+                          console.log("success: "+data[0]);
+                          Streamer.initialize(
+                             {
+                                 element: "livestream",
+                                 host: "<?=parse_url(URL::to('/'), PHP_URL_HOST)?>",
+                                 port: data[0],
+                                 width: '100%',
+                                 callback: function(){}
+                             });
+                          })
+
+                          Sequencer.initialize(
                             {
                                 element: "canvas",
                                 direction:"-x",
