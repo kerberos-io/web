277f74d524e9ccd90bf0c72226c7347a8c46aaf4
Mikael Hadler <mikaelhadler@gmail.com>
2016-10-17 02:04:10 -0200

    Update cloud.php
    correct space word

--- app/lang/pt/cloud.php
+++ app/lang/pt/cloud.php
@@ -13,7 +13,7 @@ return array(
     
     'cloud' => 'Nuvem',
     
-    'description' => 'Kerberos.io oferece um <b>serviço de aplicação em nuvem</b>, para centralizare acompanhar varias intâncias Kerberos.io. Para sincronizar os eventos desta instância para o nosso aplicativo em nuvem, primeiro você precisa <a href="https://cloud.kerberos.io">registrar uma conta</a> e escolher uma plano de assinatura. Se o registro for feito com sucesso, você receberá credenciais que poderão ser preenchidas nos campos abaixo. Depois de inserir <b>suas credenciais</b>, seus eventos serão enviados para a aplicação em nuvem',
+    'description' => 'Kerberos.io oferece um <b>serviço de aplicação em nuvem</b>, para centralizar e acompanhar varias intâncias Kerberos.io. Para sincronizar os eventos desta instância para o nosso aplicativo em nuvem, primeiro você precisa <a href="https://cloud.kerberos.io">registrar uma conta</a> e escolher uma plano de assinatura. Se o registro for feito com sucesso, você receberá credenciais que poderão ser preenchidas nos campos abaixo. Depois de inserir <b>suas credenciais</b>, seus eventos serão enviados para a aplicação em nuvem',
     
 	'update' => 'Atualizar',
 
