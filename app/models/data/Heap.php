46194edcaf29035ffece9ee3679ef8a574646ed3
Cédric Verstraeten <hello@cedric.ws>
2016-12-14 19:39:44 +0100

    sorting was not working properly

--- app/models/data/Heap.php
+++ app/models/data/Heap.php
@@ -7,6 +7,10 @@ class Heap extends SplHeap
     public function compare($a, $b)
     {
         if ($a === $b) return 0;
+
+        $a = intval(explode('_', $a)[0]);
+        $b = intval(explode('_', $b)[0]);
+
         return $a > $b ? 1 : -1;
     }
 }
