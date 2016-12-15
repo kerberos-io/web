acb850bab494b15e3b3279bc8d517c66678c08fc
Cédric Verstraeten <hello@cedric.ws>
2016-12-15 07:59:20 +0100

    sort on microseconds as well

--- app/models/data/Heap.php
+++ app/models/data/Heap.php
@@ -8,9 +8,22 @@ class Heap extends SplHeap
     {
         if ($a === $b) return 0;
 
-        $a = intval(explode('_', $a)[0]);
-        $b = intval(explode('_', $b)[0]);
+        $partsA = explode('_', $a);
+        $partsB = explode('_', $b);
 
-        return $a > $b ? 1 : -1;
+        $timeA = intval($partsA[0]);
+        $timeB = intval($partsB[0]);
+
+        if($timeA == $timeB)
+        {
+        	$microA = $partsA[1];
+        	$microB = $partsB[1];
+        	
+        	return $microA > $microB ? 1 : -1;
+        }
+        else
+        {
+        	return $timeA > $timeB ? 1 : -1;
+        }
     }
 }
