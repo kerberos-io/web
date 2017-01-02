225109c15f172649063d380d9929279d2ab91912
Martin <martin@stolle.de>
2017-01-02 20:47:45 +0100

    added German translation

---
+++ app/lang/de/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passwörter müssen mindestens 6 Zeichen lang sein und mit dem Bestätigungsfeld übereinstimmen.",
+
+	"user" => "Wir können keinen Benutzer mit der E-Mail Adresse finden.",
+
+	"token" => "Der Reset-Token des Passwortes ist ungültig.",
+
+	"sent" => "Passworterinnerung wurde gesendet!",
+
+);
