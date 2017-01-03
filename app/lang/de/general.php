7062177f752efdc6c75be4e872e706b8ec895a9f
Martin <lrhodes@outlook.de>
2017-01-03 21:12:05 +0100

    Added german translation (#66)
    * added German translation
    
    * Added translation

---
+++ app/lang/de/general.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| General Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the master template.
+	|
+	*/
+
+	'dashboard' => 'Dashboard',
+
+	'system' => 'System',
+
+	'settings' => 'Einstellungen',
+
+	'cloud' => 'Cloud',
+    
+	'on' => 'An',
+    
+	'off' => 'Aus',
+    
+	'logout' => 'Abmelden',
+    
+    
+    /*
+	|------------------------
+    |  Date-time translations
+	|------------------------
+    */
+
+    'monday' => 'Montag',
+
+    'tuesday' => 'Dienstag',
+
+    'wednesday' => 'Mittwoch',
+
+    'thursday' => 'Donnerstag',
+
+    'friday' => 'Freitag',
+
+    'saturday' => 'Samstag',
+
+    'sunday' => 'Sonntag',
+            
+    'today' => 'Heute',
+    
+    'yesterday' => 'Gestern',
+    
+    'dayBeforeYesterday' => 'Vorgestern'
+
+);
---
+++ app/lang/de/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Settings Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the settings page.
+	|
+	*/
+    
+    'settings' => 'Einstellungen',
+
+    'configuration' => 'Konfiguration',
+
+	'update' => 'Aktualisieren',
+
+);
