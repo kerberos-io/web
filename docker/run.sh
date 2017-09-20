5b2f3e43d1b41eb7a515175987e579f7ddc9723a
William Comartin <wcomartin@fullcircletms.com>
2017-09-19 22:19:13 -0400

    Add Docker Env Variables for Machinery Host and Port

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i "s|machinery:8889|$MACHINERY_HOST:$MACHINERY_PORT|g" /etc/nginx/sites-available/default.conf
+
 echo "[www]" > /etc/php/7.0/fpm/pool.d/env.conf
 echo "" >> /etc/php/7.0/fpm/pool.d/env.conf
 env | grep "KERBEROSIO_" | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" >> /etc/php/7.0/fpm/pool.d/env.conf
