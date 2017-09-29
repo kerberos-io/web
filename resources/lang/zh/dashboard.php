738387b5c5512751cdf7f6175656a75a87fccba5
MCRy <578216802@qq.com>
2017-09-29 15:01:20 +0800

    added simplified chinese translation

---
+++ resources/lang/zh/dashboard.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Dashboard Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the dashboard page.
+	|
+	*/
+
+	'activity' => '活动',
+
+	'selectView' => '界面选择',
+
+	'liveView' => '直播界面',
+
+	'lastActivity' => '最近活动',
+
+	'heatmap' => '热力图',
+
+	'overview' => '总览',
+
+	'hour' => '小时',
+
+  'average' => '平均',
+
+	'weekday' => '工作日',
+
+);
---
+++ resources/lang/zh/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "密码至少六位，且前后一致。",
+
+	"user" => "提供的邮箱地址无法找到。",
+
+	"token" => "密码重置口令已过期。",
+
+	"sent" => "密码提示已发送！",
+
+);
