38883e37e69d31d6e7ad716fa572016b49afc4f1
RayX <MCRy@users.noreply.github.com>
2017-09-29 15:18:17 +0800

    added simplified chinese translation (#98)

---
+++ resources/lang/zh/general.php
@@ -0,0 +1,58 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| General Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the master template.
+	|
+	*/
+
+	'dashboard' => '面板',
+
+	'system' => '系统',
+
+	'settings' => '设置',
+
+	'configuration' => '配置',
+
+	'cloud' => '云端',
+    
+	'on' => '开',
+    
+	'off' => '关',
+    
+	'logout' => '登出',
+
+	'update-profile' => '更新个人资料',
+    
+    /*
+	|------------------------
+    |  Date-time translations
+	|------------------------
+    */
+
+    'monday' => '周一',
+
+    'tuesday' => '周二',
+
+    'wednesday' => '周三',
+
+    'thursday' => '周四',
+
+    'friday' => '周五',
+
+    'saturday' => '周六',
+
+    'sunday' => '周日',
+            
+    'today' => '今天',
+    
+    'yesterday' => '昨天',
+    
+    'dayBeforeYesterday' => '前天'
+
+);
---
+++ resources/lang/zh/updateprofile.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Update profile Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the update profile model.
+	|
+	*/
+
+	'updateProfile' => '更新个人资料', 
+
+	'username' => '您的用户名',
+
+	'language' => '语言',
+
+	'languageInfo' => 'Is your language missing or incomplete? Contribute your language to our Github repository.',
+
+	'password' => '密码',
+
+	'newPassword' => '新密码',
+
+	'changePassword' => '更改密码',
+
+	'currentPassword' => '当前密码',
+
+	'newPassword' => '新密码',
+
+	'confirmNewPassword' => '确认新密码',
+
+	'cancel' => '取消',
+
+	'update' => '更新'
+
+);
