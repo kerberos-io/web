022e1cb74cc25f5b29943fe27902e2be44b175fb
Cédric Verstraeten <hello@cedric.ws>
2018-02-05 22:33:31 +0100

    set en

--- config/kerberos.php
+++ config/kerberos.php
@@ -2,16 +2,16 @@
   'type' => 'basic',
   'radius' => '1',
   'installed' => false,
-  'users' => 
+  'users' =>
   array (
-    0 => 
+    0 =>
     array (
       'id' => 1,
       'username' => 'root',
       'password' => 'root',
       'firstname' => 'Firstname',
       'secondname' => 'Lastname',
-      'language' => 'nl',
+      'language' => 'en',
     ),
   ),
 );
