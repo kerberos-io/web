780214551aeba7938cb4fd22c4d1c745cb95bedd
Cedric Verstraeten <cedric@verstraeten.io>
2020-04-17 22:46:54 +0200

    remove docker from sub projects.

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "[www]" > /etc/php/7.0/fpm/pool.d/env.conf
-echo "" >> /etc/php/7.0/fpm/pool.d/env.conf
-env | grep "KERBEROSIO_" | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" >> /etc/php/7.0/fpm/pool.d/env.conf
-service php7.0-fpm start
-
-/usr/bin/supervisord -n -c /etc/supervisord.conf
