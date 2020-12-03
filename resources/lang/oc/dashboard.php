e6020c57e2e1116ebdf615c6b9bb955cc39e97b8
Mejans <61360811+Mejans@users.noreply.github.com>
2020-12-03 20:07:33 +0100

    Create dashboard.php

---
+++ resources/lang/oc/dashboard.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Dashboard Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the dashboard page.
+	|
+	*/
+
+	'activity' => 'Activitat',
+
+	'selectView' => 'Seleccionar vista',
+
+	'liveView' => 'Vista del dirèct',
+
+	'lastActivity' => 'Darrièra activitat',
+
+	'heatmap' => 'Mapa de calor',
+
+	'overview' => 'Apercebut',
+
+	'hour' => 'Vista per ora',
+
+  'average' => 'Vista mejana',
+
+	'weekday' => 'Vista per setmana',
+
+);
