9df3f6c5c427a16e86d26bb1cddca5c1553d32cb
Mejans <61360811+Mejans@users.noreply.github.com>
2020-12-03 20:10:33 +0100

    Create general.php

---
+++ resources/lang/oc/general.php
@@ -0,0 +1,58 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| General Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the master template.
+	|
+	*/
+
+	'dashboard' => 'Dashboard',
+
+	'system' => 'System',
+
+	'settings' => 'Settings',
+
+	'configuration' => 'Configuration',
+
+	'cloud' => 'Cloud',
+    
+	'on' => 'On',
+    
+	'off' => 'Off',
+    
+	'logout' => 'Logout',
+
+	'update-profile' => 'Update profile',
+    
+    /*
+	|------------------------
+    |  Date-time translations
+	|------------------------
+    */
+
+    'monday' => 'Diluns',
+
+    'tuesday' => 'Dimars',
+
+    'wednesday' => 'Dimècres',
+
+    'thursday' => 'Dijòus',
+
+    'friday' => 'Divendres',
+
+    'saturday' => 'Dissabte',
+
+    'sunday' => 'Dimenge',
+            
+    'today' => 'Uèi',
+    
+    'yesterday' => 'Ièr',
+    
+    'dayBeforeYesterday' => 'Passat ièr'
+
+);
